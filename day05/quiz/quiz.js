d421c92b0a96e831fe438d973b5a82ff3b06feff
yonghyuny <159427668+yonghyuny@users.noreply.github.com>
2024-05-20 13:46:48 +0900

    Add files via upload

--- day05/quiz/quiz.js
+++ day05/quiz/quiz.js
@@ -3,16 +3,18 @@ const menu = [];
 // 메뉴를 넣고 제출 버튼누르면
 // menu에 글이 누적되면서 추가됩니다.
 
-const menuInput = document.querySelector(".menuInput");
-const submit = document.querySelector(".submit");
+const menuInput = document.querySelector('.menuInput');
+const submit = document.querySelector('.submit');
+const menuList = document.querySelector('.menuList');
 
-submit.addEventListener("click", () => {
+submit.addEventListener('click', () => {
   const mv = menuInput.value;
-  const menuList = document.querySelector(".menuList");
-  menuList.innerText = `${mv}`;
-  const box = document.createElement("div");
+  let addMenu = ``;
+  menuList.innerText = addMenu;
+  menu.push(`${mv}, `);
 
-  document.menuList.appendChild(box);
-  box.innerText = `${mv}`;
-  console.log(mv);
+  menu.forEach((v) => {
+    menuList.innerText = `${(addMenu += v)}`;
+  });
+  console.log(menu);
 });
