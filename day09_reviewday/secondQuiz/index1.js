2b43c5cfca363616fc3dc9404a09ed099f3ba1b3
yonghyuny <159427668+yonghyuny@users.noreply.github.com>
2024-05-24 13:55:53 +0900

    Add files via upload

--- day09_reviewday/secondQuiz/index1.js
+++ day09_reviewday/secondQuiz/index1.js
@@ -1,24 +1,24 @@
 const menus = [
-  { coffeeName: "아메리카노", coffeePrice: "3000원" },
+  { coffeeName: '아메리카노', coffeePrice: '3000원' },
   {
-    coffeeName: "라떼",
-    coffeePrice: "4000원",
+    coffeeName: '라떼',
+    coffeePrice: '4000원',
   },
   {
-    coffeeName: "레몬에이드",
-    coffeePrice: "3500원",
+    coffeeName: '레몬에이드',
+    coffeePrice: '3500원',
   },
 ];
 
 const makeName = (coffeeName) => {
-  const h3 = document.createElement("h3");
+  const h3 = document.createElement('h3');
   h3.innerText = coffeeName;
   return h3;
 };
 // 커피이름을 오브젝트에서 가져오는 함수
 
 const makePrice = (coffeePrice) => {
-  const h5 = document.createElement("h5");
+  const h5 = document.createElement('h5');
   h5.innerText = coffeePrice;
   return h5;
 };
@@ -26,11 +26,11 @@ const makePrice = (coffeePrice) => {
 // 커피 가격을 오브젝트에서 가져오는 함수
 
 const makeButton = (coffeePrice) => {
-  const btn = document.createElement("button");
-  btn.innerText = "추가하기";
-  btn.addEventListener("click", () => {
-    const total = document.querySelector(".total");
-    total.innerText = parseInt(total.innerText) + coffeePrice + "원";
+  const btn = document.createElement('button');
+  btn.innerText = '추가하기';
+  btn.addEventListener('click', () => {
+    const total = document.querySelector('.total');
+    total.innerText = parseInt(total.innerText) + coffeePrice + '원';
   });
   return btn;
 };
@@ -39,14 +39,39 @@ const makeButton = (coffeePrice) => {
 // html total클래스에 누른 가격버튼에 대한 추가 가격을 출력하는 함수
 
 const makeCoffee = (coffeeObj) => {
-  const coffeeBox = document.createElement("div");
+  const coffeeBox = document.createElement('div');
   coffeeBox.appendChild(makeName(coffeeObj.coffeeName));
   coffeeBox.appendChild(makePrice(coffeeObj.coffeePrice));
   coffeeBox.appendChild(makeButton(coffeeObj.coffeePrice));
-  document.querySelector(".menu").appendChild(coffeeBox);
+  document.querySelector('.menu').appendChild(coffeeBox);
 };
 // 개별의 이름 가격 버튼 의 함수를 다시 함수화 해서
 //  붙히는 기능
 
 menus.forEach((v) => makeCoffee(v));
 // 메뉴안의
+
+// ------------------------------------------------
+// const total1 = document.querySelector('.total');
+// let total = 0;
+
+// menus.forEach((v) => {
+//   const h3 = document.createElement('h3');
+//   h3.innerText = v.coffeeName;
+
+//   const h5 = document.createElement('h5');
+//   h5.innerText = v.coffeePrice;
+
+//   const btn = document.createElement('button');
+//   btn.innerText = '추가하기';
+
+//   btn.addEventListener('click', () => {
+//     total1.innerText = `total : ${(total += v.coffeePrice)} 원`;
+//   });
+
+//   const menu = document.querySelector('.menu');
+
+//   menu.appendChild(h3);
+//   menu.appendChild(h5);
+//   menu.appendChild(btn);
+// });
